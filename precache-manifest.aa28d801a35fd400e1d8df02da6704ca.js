f81a0af474b9b80d39089bf42a57a2726d1d41c1
Mateusz Mikulski <mikulskee@gmail.com>
2019-09-08 14:04:43 +0200

    Updates

---
+++ precache-manifest.aa28d801a35fd400e1d8df02da6704ca.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f261d56676aac92076688b482331b24f",
+    "url": "/ChangingRoom/index.html"
+  },
+  {
+    "revision": "5bd408a9998085bc3fbe",
+    "url": "/ChangingRoom/static/css/main.b8fe2e41.chunk.css"
+  },
+  {
+    "revision": "ea687e4b6e96b686479b",
+    "url": "/ChangingRoom/static/js/2.2588c173.chunk.js"
+  },
+  {
+    "revision": "5bd408a9998085bc3fbe",
+    "url": "/ChangingRoom/static/js/main.1b30737e.chunk.js"
+  },
+  {
+    "revision": "3ac7c76fbe6c078deac8",
+    "url": "/ChangingRoom/static/js/runtime~main.821f2008.js"
+  },
+  {
+    "revision": "47a0ef57acea3b83f9c58d620fbb1bed",
+    "url": "/ChangingRoom/static/media/additives.47a0ef57.jpg"
+  },
+  {
+    "revision": "5b1413a8324232e7e62d2fa7d71880a4",
+    "url": "/ChangingRoom/static/media/contact-image.5b1413a8.jpg"
+  },
+  {
+    "revision": "f371b59c8f08553511930f740ea168f8",
+    "url": "/ChangingRoom/static/media/header1.f371b59c.jpg"
+  },
+  {
+    "revision": "5d125a1957322d87673dba4a7fde3329",
+    "url": "/ChangingRoom/static/media/hoodies.5d125a19.jpg"
+  },
+  {
+    "revision": "66a5790b8be398e5c5b8e90d8eff2cf9",
+    "url": "/ChangingRoom/static/media/jackets.66a5790b.jpg"
+  },
+  {
+    "revision": "57ac71746c8d609efd9470cdd790a022",
+    "url": "/ChangingRoom/static/media/logo-white.57ac7174.png"
+  },
+  {
+    "revision": "cd39ab474b890f4a26f0d8fa6612a0ec",
+    "url": "/ChangingRoom/static/media/logo.cd39ab47.png"
+  },
+  {
+    "revision": "e1fb52c9537e872145c184bd595b347a",
+    "url": "/ChangingRoom/static/media/pants.e1fb52c9.jpg"
+  },
+  {
+    "revision": "c147a7fe366cfc92a42c176dccd5bc96",
+    "url": "/ChangingRoom/static/media/shirts.c147a7fe.jpg"
+  },
+  {
+    "revision": "66eb781f4f9a443431e34e9edf2f49d9",
+    "url": "/ChangingRoom/static/media/shoes.66eb781f.jpg"
+  },
+  {
+    "revision": "4430b3cae9b34ce9e7c7977b2bf329a7",
+    "url": "/ChangingRoom/static/media/skirts.4430b3ca.jpg"
+  },
+  {
+    "revision": "9ca15ce85177f7cd51ab917bb9a4e80b",
+    "url": "/ChangingRoom/static/media/t-shirts.9ca15ce8.jpg"
+  }
+]);
